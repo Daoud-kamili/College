7991b73bf195b8cb64872564476f943dc0785377
Daoud-kamili <daoudkamili2929@gmail.com>
2025-04-25 15:35:23 +0530

    Add files via upload

---
+++ area and perimeter of a rectangle.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int area;
+int perimeter;
+int l;
+int b;
+printf("enter the length of rectangle \n");
+scanf("%d",&l);
+printf("enter the breadth of rectangle \n");
+scanf("%d",&b);
+area=l*b;
+perimeter= 2*(l+b);
+printf("the area of rectangle is %d", area);
+printf("the perimeter of rectagle is %d", perimeter);
+}
