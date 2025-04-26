90ab818c76fc8819a5cb8790b3456eec4fb11ca9
Daoud Kamili <daoudkamili2929@gmail.com>
2025-04-26 21:59:00 +0530

    Update area and perimeter of a rectangle.cpp

--- area and perimeter of a rectangle.cpp
+++ area and perimeter of a rectangle.cpp
@@ -10,6 +10,6 @@ printf("enter the breadth of rectangle \n");
 scanf("%d",&b);
 area=l*b;
 perimeter= 2*(l+b);
-printf("the area of rectangle is %d", area);
+printf("the area of rectangle is %d\n", area);
 printf("the perimeter of rectagle is %d", perimeter);
 }
