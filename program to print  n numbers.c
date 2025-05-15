9cdb7fc54b91783b672ef986157bb5a84227f668
Daoud Kamili <daoudkamili2929@gmail.com>
2025-05-15 12:54:51 +0530

    Add files via upload

---
+++ program to print  n numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int i=0;
+	int n;
+	printf("enter upto which no. you want to print\n");
+	scanf("%d",&n);
+	do
+	{
+	printf("%d\n",i);
+		i++;
+	}
+	while(i<=n);
+		}
