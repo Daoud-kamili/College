25c2dbb4c5345a645f247195a7234f2df1a21427
Daoud Kamili <daoudkamili2929@gmail.com>
2025-05-25 11:33:04 +0530

    Add files via upload

---
+++ product of numbers from 1 to 100.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int pr;
+    int n;
+    
+    printf("enter the no. whose factorila you want to find\n");
+    scanf("n");
+
+    
+    for (int i = 1; i <= i<=n; i++) {
+        pr *= i;
+    }
+
+    
+    printf("The factorial of numbers from 1 to 100 is: %d\n", &pr);
+
+    return 0;
+}
