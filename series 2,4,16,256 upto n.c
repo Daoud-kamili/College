f8a87826c89ecc7f2a94243e8a358edba22a2210
Daoud Kamili <daoudkamili2929@gmail.com>
2025-07-19 11:30:37 +0530

    Add files via upload

---
+++ series 2,4,16,256 upto n.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,term=2;
+	printf("enter the limit of series :- \n");
+	scanf("%d",&n);
+	//int sum=0;
+	for(i=1;i<=n;i++)
+	{
+		printf("%d ",term);
+		term=term*term;
+		
+		
+	}
+	
+}
